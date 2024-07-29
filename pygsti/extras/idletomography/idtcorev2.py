d3979b0562f78636659fb61c4a9b6180e7e5d3f4
Kyle Skolfield <kyle.skolfield@asu.edu>
2024-07-29 15:01:56 -0600

    ick pandas

--- pygsti/extras/idletomography/idtcorev2.py
+++ pygsti/extras/idletomography/idtcorev2.py
@@ -16,6 +16,7 @@ import numpy as _np
 from pygsti.baseobjs import Basis
 import re
 from icecream import ic
+from ordered_set import OrderedSet
 
 import logging
 
@@ -375,7 +376,7 @@ def jacobian_coefficient_calc(error_gen_type, pauli_index, prep_string, meas_str
 
 
 if __name__ == "__main__":
-    num_qubits = 1
+    num_qubits = 2
     max_weight = 1
 
     HS_index_iterator = stim.PauliString.iter_all(
@@ -432,10 +433,12 @@ if __name__ == "__main__":
 
     import pandas as pd
 
-    df = pd.DataFrame()
-    
+    # df = pd.DataFrame()
+    jacobian_coef_dict = {"index": OrderedSet(), "columns": OrderedSet()}
+    data = {}
 
     # These come back as class, index, prep_str, meas_str, observ_str: coef
+    # I THINK this is right, should double check and write unit tests
     for key in hs_experiment + ca_experiment:
         elt = jacobian_coefficient_calc(*key)
         for el in elt:
@@ -443,49 +446,78 @@ if __name__ == "__main__":
                 observable = ",".join(str(s)[1:] for s in el[-4:-1])
                 egen = ",".join(str(s) for s in el[:-4])
                 coef = int(el[-1])
-                df.at[observable, egen] = coef
-    #df.at causes fragmentation errors to be spammed
+                jacobian_coef_dict["index"].add(observable)
+                jacobian_coef_dict["columns"].add(egen)
+                if data.get(egen):
+                    data[egen].append(coef)
+                else:
+                    data[egen] = [coef]
 
-    whatever = df.to_numpy()
-    inv = _np.linalg.pinv(whatever)
-    ic(whatever)
-    ic(inv)
-
-
-
-
+    df = pd.DataFrame(data, index= jacobian_coef_dict["index"], columns=jacobian_coef_dict["columns"])
+    ic(df)
 
 
-    quit()
+    # whatever = df.to_numpy()
+    # inv = _np.linalg.pinv(whatever)
+    # ic(whatever)
+    # ic(inv)
 
-    hs_experiment = list(
-        product(
-            hs_error_gen_classes,
-            pauli_node_attributes,
-            measure_string_attributes,
-            measure_string_attributes,
-        )
-    )
-    ca_experiment = list(
-        product(
-            ca_error_gen_classes,
-            ca_pauli_node_attributes,
-            measure_string_attributes,
-            measure_string_attributes,
-        )
-    )
 
-    jacobian_coefficient_dict = {}
 
-    # These come back as class, index, prep_str, meas_str, observ_str: coef
-    for key in hs_experiment + ca_experiment:
-        elt = jacobian_coefficient_calc(*key)
-        # ic(elt)
-        for el in elt:
-            # ic(el)
-            if el:
-                jacobian_coefficient_dict[tuple(str(e) for e in el[:-1])] = int(el[-1])
-                # ic(jacobian_coefficient_dict)
 
-    for k,v in jacobian_coefficient_dict.items():
-        ic(k,v)
+
+
+    # quit()
+
+    # hs_experiment = list(
+    #     product(
+    #         hs_error_gen_classes,
+    #         pauli_node_attributes,
+    #         measure_string_attributes,
+    #         measure_string_attributes,
+    #     )
+    # )
+    # ca_experiment = list(
+    #     product(
+    #         ca_error_gen_classes,
+    #         ca_pauli_node_attributes,
+    #         measure_string_attributes,
+    #         measure_string_attributes,
+    #     )
+    # )
+
+    # jacobian_coefficient_dict = {"H": [], "S": [], "C": [], "A": []}
+    # experiment_set = OrderedSet()
+
+    # egen_dict = {"H": OrderedSet(), "S": OrderedSet(), "C": OrderedSet(), "A": OrderedSet()}
+    
+    
+    # # These come back as class, index, prep_str, meas_str, observ_str: coef
+    # for key in hs_experiment + ca_experiment:
+    #     elt = jacobian_coefficient_calc(*key)
+    #     # ic(elt)
+    #     for el in elt:
+    #         # ic(el)
+    #         if el:
+    #             experiment = ",".join(str(s)[1:] for s in el[-4:-1])
+    #             observable = str(el[-2])[1:]
+    #             egen = ",".join(str(s) for s in el[:-4])
+    #             egen_class = el[0]
+    #             coef = int(el[-1])
+    #             jacobian_coefficient_dict[egen_class].append(coef)
+    #             experiment_set.add(experiment)
+
+    #             egen_dict[egen_class].add(egen)
+    #             # ic(jacobian_coefficient_dict)
+
+    # egen_list = _np.array([v for v in egen_dict.values()]).flatten()
+    # # for k,v in jacobian_coefficient_dict.items():
+    # #     ic(k,v)
+    # h_matrix = _np.array(jacobian_coefficient_dict["H"]).reshape(len(experiment_set), len(egen_dict["H"]))
+    # s_matrix = _np.array(jacobian_coefficient_dict["S"]).reshape(len(experiment_set), len(egen_dict["S"]))
+    # c_matrix = _np.array(jacobian_coefficient_dict["C"]).reshape(len(experiment_set), len(egen_dict["C"]))
+    # a_matrix = _np.array(jacobian_coefficient_dict["A"]).reshape(len(experiment_set), len(egen_dict["A"]))
+    # jacobian = _np.concatenate([h_matrix,s_matrix, c_matrix,a_matrix], axis=1)
+    # ic(jacobian)
+    # df = pd.DataFrame(jacobian, index=experiment_set, columns=egen_list)
+    # ic(df)
